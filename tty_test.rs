61577cf0d6bad170a4e007aa7ef8fcf11be22682
dia <dia17461@diaestudents.com>
2025-11-10 21:56:31 +0400

    Remove bloatware: clean up project by removing excessive tests, docs, and CI workflows

--- tty_test.rs
+++
@@ -1,7 +0,0 @@
-use atty;
-
-fn main() {
-    println!("stdin is tty: {}", atty::is(atty::Stream::Stdin));
-    println!("stdout is tty: {}", atty::is(atty::Stream::Stdout));
-    println!("stderr is tty: {}", atty::is(atty::Stream::Stderr));
-}
